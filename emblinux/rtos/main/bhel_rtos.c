7a3599732f59981930c9bbf847bf4daae711644e
Mahesh Patil <cpuinfo10@gmail.com>
2022-11-03 17:15:58 +0530

    MQ

--- emblinux/rtos/main/bhel_rtos.c
+++ emblinux/rtos/main/bhel_rtos.c
@@ -1,34 +1,46 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 
-TaskHandle_t sensor_task_handle;
+TaskHandle_t task_handle1, task_handle2;
+QueueHandle_t locq_handle;
 
-void sensor_task(void *pvParameters)
+void location_task(void *pvParameters)
 {
+    int loc = 0;
     while (1)
     {
-        printf("Sensor task: entry\n");
-        vTaskSuspend(NULL);
-        // ---------
+        loc++;
+        printf("Location task: entry\n");
+        xQueueSend(locq_handle, &loc, (TickType_t)10);
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
 
+void location_processor(void *pvParameters)
+{
+    int buff;
+    while (1)
+    {
+        printf("Location Processor task: entry\n");
+        xQueueReceive(locq_handle, &buff, (TickType_t)10);
+        printf("Loc: %d\n", buff);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
 void app_main(void)
 {
     int count = 0;
     printf("Hello BHEL on ESP32\n");
 
-    xTaskCreate(sensor_task, "sensor_task", 1024, NULL, 10, &sensor_task_handle);
+    locq_handle = xQueueCreate(5, sizeof(int));
+    xTaskCreate(location_task, "sensor_task", 2048, NULL, 10, &task_handle2);
+    xTaskCreate(location_processor, "sensor_task", 2048, NULL, 12, &task_handle1);
 
     while (1)
     {
         count++;
-        if (count == 10)
-        {
-            vTaskResume(sensor_task_handle);
-        }
         printf("Main thread\n");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
